a3085bfeb3a7c847f0bc07dd2313f49e4f085e21
paulodeleso <69392316+paulodeleso@users.noreply.github.com>
2020-08-12 20:35:35 +0800

    1.0

---
+++ System Monitor/main.js
@@ -0,0 +1,36 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+//init win
+let win;
+
+function createWindow(){
+    // Create BrowserWindow
+    win = new BrowserWindow({width:800, height:600});
+
+
+    // Load Index.html
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    // Open Devtools
+    win.webContents.openDevTools();
+
+    win.on('closed', () => {
+        win = null;
+    });
+}
+
+// Run create window function
+app.on('ready', createWindow);
+
+// Quit when all windows are closed
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin'){
+        app.quit();
+    }
+});
